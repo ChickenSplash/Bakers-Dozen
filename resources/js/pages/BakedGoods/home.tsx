cfed4efb430a0f19a22a419b80f66eaa23f3beac
Emanuel Correia <guardeded@gmail.com>
2025-10-27 16:07:16 +0000

    added is_admin to users table

--- resources/js/pages/BakedGoods/home.tsx
+++ resources/js/pages/BakedGoods/home.tsx
@@ -1,11 +1,36 @@
-import { BakedGoodResource } from "@/types";
+import { login, register } from "@/routes";
+import { BakedGoodResource, SharedData } from "@/types";
+import { Head, Link, usePage } from "@inertiajs/react";
 
 export default function Home({bakedGoods}: {bakedGoods: BakedGoodResource}) {
+    const { auth } = usePage<SharedData>().props;
+
     return (
-        <div>
-            <h1>Baked Goods Home</h1>
-            <pre>{JSON.stringify(bakedGoods, null, 2)}</pre>
-            {/* Use bakedGoods to iterate and display baked goods here. */}
-        </div>
+        <>
+            <Head title="Welcome">
+                <link rel="preconnect" href="https://fonts.bunny.net" />
+                <link
+                    href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600"
+                    rel="stylesheet"
+                />
+            </Head>
+            <div>
+                <Link
+                    href={login()}
+                    className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
+                >
+                    Log in
+                </Link>
+                <Link
+                    href={register()}
+                    className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
+                >
+                    Register
+                </Link>
+                <h1>Baked Goods Home</h1>
+                <pre>{JSON.stringify(bakedGoods, null, 2)}</pre>
+                {/* Use bakedGoods to iterate and display baked goods here. */}
+            </div>
+        </>
     );
 }
